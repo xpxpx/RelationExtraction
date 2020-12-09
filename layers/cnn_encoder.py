b76b5ec7844d2c4e8ec87a36084f9e49ce23b9e2
xueping <xue.ping@pku.edu.cn>
2020-12-09 14:04:36 +0800

    add

--- layers/cnn_encoder.py
+++ layers/cnn_encoder.py
@@ -6,6 +6,12 @@ class CNNEncoder(nn.Module):
     def __init__(self, input_dim, hidden_dim, kernel_size=3):
         super(CNNEncoder, self).__init__()
         self.encoder = nn.Conv1d(input_dim, hidden_dim, kernel_size=kernel_size, padding=(kernel_size - 1) // 2)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.xavier_normal_(self.encoder.weight.data, gain=nn.init.calculate_gain('relu'))
 
     def forward(self, token):
-        pass
+        encoder_output = torch.relu(self.encoder(token.transpose(1, 2)))
+        pool_output, _ = torch.max(encoder_output, dim=2)
+        return pool_output
--- models/cnn.py
+++ models/cnn.py
@@ -1,10 +1,50 @@
 import torch
 import torch.nn as nn
+from layers.embedding import Embedding
+from layers.cnn_encoder import CNNEncoder
+from utils.constant import PAD, MAX_LEN
 
 
 class CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, config):
         super(CNN, self).__init__()
+        self.word_emb = Embedding(
+            config.word_vocab.size(),
+            config.word_embedding_dim,
+            padding_idx=config.word_vocab.get_index(PAD),
+            pretrain_embedding=config.pretrain_word_embedding
+        )
+        self.head_position_emb = Embedding(
+            config.head_position_vocab.size(),
+            config.position_embedding_dim,
+            padding_idx=config.head_position_vocab.get_index(MAX_LEN)
+        )
+        self.tail_position_emb = Embedding(
+            config.tail_position_vocab.size(),
+            config.position_embedding_dim,
+            padding_idx=config.tail_position_vocab.get_index(MAX_LEN)
+        )
 
-    def forward(self):
-        pass
+        self.encoder = CNNEncoder(
+            config.word_embedding_dim + 2 * config.position_embedding_dim,
+            config.hidden_dim,
+            kernel_size=config.kernel_size
+        )
+
+        self.linear = nn.Linear(config.hidden_dim, config.relation_vocab.size())
+
+        self.input_drop = nn.Dropout(config.input_dropout)
+        self.hidden_drop = nn.Dropout(config.hidden_dropout)
+
+    def forward(self, inputs):
+        word_embed = self.word_emb(inputs['word'])
+        head_position_embed = self.head_position_emb(inputs['head_position'])
+        tail_position_embed = self.tail_position_emb(inputs['tail_position'])
+        total_embed = self.input_drop(torch.cat([word_embed, head_position_embed, tail_position_embed], dim=-1))
+
+        hidden = self.encoder(total_embed)
+        hidden = self.hidden_drop(hidden)
+
+        logits = self.linear(hidden)
+        pred = torch.argmax(logits, dim=-1)
+        return logits, pred
