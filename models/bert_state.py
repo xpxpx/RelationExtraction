21f1cbd6beb9dddcec877fdeeab62f73a6ca9013
xueping <xue.ping@pku.edu.cn>
2020-12-09 17:24:16 +0800

    add

---
+++ models/bert_state.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+
+class BERTState(nn.Module):
+    def __init__(self, config):
+        super(BERTState, self).__init__()
+
+    def forward(self):
+        pass
--- process.py
+++ process.py
@@ -1,6 +1,8 @@
 import re
 import json
+from tqdm import tqdm
 import jsonlines as jl
+from transformers import AutoTokenizer
 
 
 def clean(text):
@@ -38,36 +40,6 @@ def clean(text):
 
 
 def process_semeval(input_file, output_file, relation_file=None):
-    relation_set = set()
-    lines = open(input_file, 'r', encoding='utf=8').readlines()
-    with jl.open(output_file, 'w') as f:
-        for index in range(0, len(lines), 4):
-            line = lines[index].strip().split('\t')[1][1:-1]
-            line = line.replace("<e1>", " <e1> ")
-            line = line.replace("<e2>", " <e2> ")
-            line = line.replace("</e1>", " </e1> ")
-            line = line.replace("</e2>", " </e2> ")
-
-            line = clean(line)
-            token = line.split(' ')
-            e1_start_index, e1_end_index = token.index('<e1>'), token.index('</e1>')
-            e2_start_index, e2_end_index = token.index('<e2>'), token.index('</e2>')
-
-            relation = lines[index + 1].strip()
-
-            f.write({
-                'token': token,
-                'head': [],
-                'tail': [],
-                'relation': relation
-            })
-
-    with open(relation_file, 'w', encoding='utf-8') as f:
-        for relation in relation_set:
-            f.write(relation + '\n')
-
-
-def process_semeval_dual_relation(input_file, output_file, relation_file=None):
     relation_set = set()
     lines = open(input_file, 'r', encoding='utf-8').readlines()
     with jl.open(output_file, 'w') as f:
@@ -116,6 +88,140 @@ def process_semeval_dual_relation(input_file, output_file, relation_file=None):
                 f.write(relation + '\n')
 
 
+def process_semeval_for_bert(input_file, output_file, bert_file='./data/bert-base-uncased', relation_file=None):
+    relation_set = set()
+    tokenizer = AutoTokenizer.from_pretrained(bert_file)
+    lines = open(input_file, 'r', encoding='utf-8').readlines()
+    with jl.open(output_file, 'w') as f:
+        for index in tqdm(range(0, len(lines), 4)):
+            line = lines[index].strip().split('\t')[1][1:-1]
+            line = line.replace("<e1>", " <e1> ")
+            line = line.replace("<e2>", " <e2> ")
+            line = line.replace("</e1>", " </e1> ")
+            line = line.replace("</e2>", " </e2> ")
+
+            line = clean(line)
+            token = line.split(' ')
+
+            e1_start_index, e1_end_index = token.index('<e1>'), token.index('</e1>')
+            e2_start_index, e2_end_index = token.index('<e2>'), token.index('</e2>')
+
+            for entity_index in sorted([e1_start_index, e1_end_index, e2_start_index, e2_end_index], reverse=True):
+                token.pop(entity_index)
+
+            if e1_start_index < e2_start_index:
+                e1_start_index = e1_start_index
+                e1_end_index = e1_end_index - 1
+                e2_start_index = e2_start_index - 2
+                e2_end_index = e2_end_index - 3
+            else:
+                e2_start_index = e2_start_index
+                e2_end_index = e2_end_index - 1
+                e1_start_index = e1_start_index - 2
+                e1_end_index = e1_end_index - 3
+
+            # tokenize by bert
+            new_token = []
+            offset = 0
+            token2offset = dict()
+            for token_index, t in enumerate(token):
+                new_t = tokenizer.tokenize(t)
+                new_token.extend(new_t)
+                token2offset[token_index] = offset
+                offset += len(new_t)
+            token2offset[len(token)] = len(new_token)
+
+            e1_start_index = token2offset[e1_start_index]
+            e1_end_index = token2offset[e1_end_index]
+            e2_start_index = token2offset[e2_start_index]
+            e2_end_index = token2offset[e2_end_index]
+
+            relation = lines[index + 1].strip()
+            if relation != 'Other':
+                relation = relation.split('(')[0]
+
+            relation_set.add(relation)
+
+            f.write({
+                'token': new_token,
+                'head': [" ".join(new_token[e1_start_index:e1_end_index]), list(range(e1_start_index, e1_end_index))],
+                'tail': [" ".join(new_token[e2_start_index:e2_end_index]), list(range(e2_start_index, e2_end_index))],
+                'relation': relation
+            })
+
+    if relation_file is not None:
+        with open(relation_file, 'w', encoding='utf-8') as f:
+            for relation in relation_set:
+                f.write(relation + '\n')
+
+
+def process_semeval_for_bert_add_entity_marker(input_file, output_file, bert_file='./data/bert-base-uncased', relation_file=None):
+    relation_set = set()
+    tokenizer = AutoTokenizer.from_pretrained(bert_file)
+    lines = open(input_file, 'r', encoding='utf-8').readlines()
+    with jl.open(output_file, 'w') as f:
+        for index in tqdm(range(0, len(lines), 4)):
+            line = lines[index].strip().split('\t')[1][1:-1]
+            line = line.replace("<e1>", " <e1> ")
+            line = line.replace("<e2>", " <e2> ")
+            line = line.replace("</e1>", " </e1> ")
+            line = line.replace("</e2>", " </e2> ")
+
+            line = clean(line)
+            token = line.split(' ')
+
+            e1_start_index, e1_end_index = token.index('<e1>'), token.index('</e1>')
+            e2_start_index, e2_end_index = token.index('<e2>'), token.index('</e2>')
+
+            for entity_index in sorted([e1_start_index, e1_end_index, e2_start_index, e2_end_index], reverse=True):
+                token.pop(entity_index)
+
+            if e1_start_index < e2_start_index:
+                e1_start_index = e1_start_index
+                e1_end_index = e1_end_index - 1
+                e2_start_index = e2_start_index - 2
+                e2_end_index = e2_end_index - 3
+            else:
+                e2_start_index = e2_start_index
+                e2_end_index = e2_end_index - 1
+                e1_start_index = e1_start_index - 2
+                e1_end_index = e1_end_index - 3
+
+            # tokenize by bert
+            new_token = []
+            offset = 0
+            token2offset = dict()
+            for token_index, t in enumerate(token):
+                new_t = tokenizer.tokenize(t)
+                new_token.extend(new_t)
+                token2offset[token_index] = offset
+                offset += len(new_t)
+            token2offset[len(token)] = len(new_token)
+
+            e1_start_index = token2offset[e1_start_index]
+            e1_end_index = token2offset[e1_end_index]
+            e2_start_index = token2offset[e2_start_index]
+            e2_end_index = token2offset[e2_end_index]
+
+            relation = lines[index + 1].strip()
+            if relation != 'Other':
+                relation = relation.split('(')[0]
+
+            relation_set.add(relation)
+
+            f.write({
+                'token': new_token,
+                'head': [" ".join(new_token[e1_start_index:e1_end_index]), list(range(e1_start_index, e1_end_index))],
+                'tail': [" ".join(new_token[e2_start_index:e2_end_index]), list(range(e2_start_index, e2_end_index))],
+                'relation': relation
+            })
+
+    if relation_file is not None:
+        with open(relation_file, 'w', encoding='utf-8') as f:
+            for relation in relation_set:
+                f.write(relation + '\n')
+
+
 def process_tacred():
     pass
 
@@ -133,6 +239,6 @@ def process_embedding(input_file, output_file, embedding_dim=300):
 
 
 if __name__ == '__main__':
-    process_semeval_dual_relation('./raw_data/SemEval2010_task8_all_data/SemEval2010_task8_training/TRAIN_FILE.TXT', './data/semeval/train.jl', './data/semeval/relation.txt')
-    process_semeval_dual_relation('./raw_data/SemEval2010_task8_all_data/SemEval2010_task8_testing_keys/TEST_FILE_FULL.TXT', './data/semeval/test.jl')
+    process_semeval_for_bert('./raw_data/SemEval2010_task8_all_data/SemEval2010_task8_training/TRAIN_FILE.TXT', './data/semeval/train.bert.jl', relation_file='./data/semeval/relation.txt')
+    process_semeval_for_bert('./raw_data/SemEval2010_task8_all_data/SemEval2010_task8_testing_keys/TEST_FILE_FULL.TXT', './data/semeval/test.bert.jl')
     # process_embedding('./raw_data/embedding/glove.6B.50d.txt', './data/embedding/glove.6B.50d.jl', embedding_dim=50)
